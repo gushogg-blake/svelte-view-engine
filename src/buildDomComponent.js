c554fa8c6886bd7feae845037e7d8440ae9a9cc2
Gus Hogg-Blake <gus@gushogg-blake.com>
2019-07-11 17:35:18 +0100

    use client-side css in dev (https://github.com/rollup/rollup-plugin-svelte/issues/62)

--- src/buildDomComponent.js
+++ src/buildDomComponent.js
@@ -20,7 +20,16 @@ module.exports = async (path, name, options, cache) => {
 		plugins: [
 			svelte(merge({
 				hydratable: true,
-				css: false,
+				// TODO:
+				// client-side CSS is needed in dev because the bundle doesn't write
+				// any unchanged components' CSS when using cache (see issue below)
+				// using client-side CSS is OK for dev but not prod as you get a FOUC
+				// but we won't be using cache in prod anyway, so if the issue
+				// doesn't get resolved we could leave it like this, as it only
+				// affects dev
+				// https://github.com/rollup/rollup-plugin-svelte/issues/62
+				// if the issue is resolved it can just be false
+				css: options.svelte.dev,
 			}, options.svelte)),
 	
 			resolve({
