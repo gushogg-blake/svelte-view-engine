d993a3b802c15fce7f95d6393c46ccd09ed9bce0
Gus Hogg-Blake <gus@gushogg-blake.com>
2019-12-11 16:44:28 +0000

    expose type, dir and prebuild function (api change - original exports is now .render)

--- src/engine.js
+++ src/engine.js
@@ -3,14 +3,15 @@ let Template = require("./Template");
 let ws = require("ws");
 let fs = require("flowfs");
 
-module.exports = (opts={}) => {
+module.exports = function(opts={}) {
 	let dev = process.env.NODE_ENV !== "production";
 	
 	let options = Object.assign({
-		dir: "./pages",
+		dir: null,
 		type: "html",
 		init: true,
 		template: null,
+		componentBuilders: {},
 		watch: dev,
 		liveReload: dev,
 		liveReloadPort: 5000 + Math.floor(Math.random() * 60535),
@@ -34,53 +35,68 @@ module.exports = (opts={}) => {
 		watch: options.watch,
 	});
 	
-	let createPage = (path) => {
+	function createPage(path) {
 		return new Page(template, path, options, liveReloadSocket);
 	}
 	
-	if (options.init) {
-		(async () => {
-			let files = await fs(options.dir).glob("**/*." + options.type);
+	/*
+	prebuild function is called if the init option is true.  it is also
+	exposed for more fine-grained control, e.g. a script that prebuilds the
+	pages then exits to free up memory
+	*/
+	
+	async function prebuild() {
+		let files = await fs(options.dir).glob("**/*." + options.type);
+		
+		for (let node of files) {
+			let page = createPage(node.path);
 			
-			for (let node of files) {
-				let page = createPage(node.path);
-				
-				pages[node.path] = page;
-				
-				page.build();
-			}
-		})();
+			pages[node.path] = page;
+			
+			page.build();
+		}
 	}
 	
-	return async (path, locals, callback) => {
-		let sendLocals = {};
+	if (options.init) {
+		prebuild();
+	}
+	
+	return {
+		dir: options.dir,
+		type: options.type,
 		
-		for (let p in locals) {
-			if (!options.excludeLocals.includes(p)) {
-				sendLocals[p] = locals[p];
-			}
-		}
+		prebuild,
 		
-		if (!pages[path]) {
-			pages[path] = createPage(path);
-		}
-		
-		try {
-			let result = await pages[path].render(sendLocals);
+		async render(path, locals, callback) {
+			let sendLocals = {};
 			
-			if (callback) {
-				callback(null, result);
-			} else {
-				return result;
+			for (let p in locals) {
+				if (!options.excludeLocals.includes(p)) {
+					sendLocals[p] = locals[p];
+				}
 			}
-		} catch (e) {
-			delete pages[path];
 			
-			if (callback) {
-				callback(e);
-			} else {
-				throw e;
+			if (!pages[path]) {
+				pages[path] = createPage(path);
 			}
-		}
+			
+			try {
+				let result = await pages[path].render(sendLocals);
+				
+				if (callback) {
+					callback(null, result);
+				} else {
+					return result;
+				}
+			} catch (e) {
+				delete pages[path];
+				
+				if (callback) {
+					callback(e);
+				} else {
+					throw e;
+				}
+			}
+		},
 	}
 }
