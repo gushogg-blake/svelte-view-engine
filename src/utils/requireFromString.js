33156f7ebac490cb1e89b49e965f3eb6e2eb7fa7
Gus Hogg-Blake <gus@gushogg-blake.com>
2020-07-18 09:20:25 +0100

    add pid to requireFromString filename to avoid conflicts if clustering

--- src/utils/requireFromString.js
+++ src/utils/requireFromString.js
@@ -3,6 +3,8 @@ let fs = require("flowfs");
 module.exports = async function(code, path) {
 	let tmpFile = fs(path);
 	
+	tmpFile = tmpFile.sibling(process.pid + "." + tmpFile.name);
+	
 	await tmpFile.write(code);
 	
 	delete require.cache[require.resolve(tmpFile.path)];
